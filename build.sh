978673a6069e4842c934795aacc8116838234f3d
Kenneth Davidson <xylene2016@protonmail.com>
2026-01-27 06:20:39 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,28 @@ export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 TZ='UTC'; export TZ
 umask 022
 
+CC="gcc"
+CXX="g++"
+LDFLAGS="-static -no-pie"
+export CC
+export CXX
+export LDFLAGS
+
 set -euo pipefail
+
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
 
 git clone https://github.com/mcmilk/7-Zip-zstd.git
 cd 7-Zip-zstd/CPP/7zip/Bundles/Alone2
-make -j$(nproc) -f makefile.gcc CC="gcc" CXX="g++" LDFLAGS="-static -no-pie" IS_X64=1
+make -j$(nproc) -f makefile.gcc IS_X64=1
 /bin/ls -la _o/
 rm -fr /tmp/_out
 mkdir /tmp/_out
 cp -vf _o/7zz /tmp/_out/7z
 chmod 0755 /tmp/_out/7z
 ./_o/7zz --version 2>&1 | sed 's/ ([^)]*)//g; s/ /-/g' > /tmp/_out/version.txt
+cat /tmp/_out/version.txt
 cd /tmp
 rm -fr "${_tmp_dir}"
 echo
