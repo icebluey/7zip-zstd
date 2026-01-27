511417475337104a98f5e88c9a6dabfd2c02947a
Kenneth Davidson <xylene2016@protonmail.com>
2026-01-27 14:23:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,10 +18,15 @@ mkdir /tmp/_out
 cp -f _o/7zz /tmp/_out/7z
 sleep 1
 chmod 0755 /tmp/_out/7z
+
 file _o/7zz
 ldd _o/7zz || true
 ./_o/7zz --version 2>&1 | sed 's/ ([^)]*)//g; s/ /-/g' > /tmp/_out/version.txt
 cat /tmp/_out/version.txt
+
+cd /tmp/_out
+openssl dgst -r -sha256 7z > 7z.sha256
+
 cd /tmp
 rm -fr "${_tmp_dir}"
 echo
