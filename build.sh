666abee2b443aa2d10f5b1bb76916c08453d9fd6
Kenneth Davidson <xylene2016@protonmail.com>
2026-01-27 16:39:08 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
 
 git clone https://github.com/mcmilk/7-Zip-zstd.git
+
 cd 7-Zip-zstd/CPP/7zip/Bundles/Alone2
 make -j$(nproc) -f makefile.gcc CC="gcc" CXX="g++" LDFLAGS="-static -no-pie" IS_X64=1
 
